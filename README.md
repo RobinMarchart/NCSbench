80519e03d027c620bac74a5483c0c4e4934c55bc
Robin Marchart <Robin.Marchart@hotmail.com>
2019-10-23 09:57:27 +0200

    fixed bootstrap.sh

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 * [General Information](https://github.com/tum-lkn/NCSbench/wiki) about NCSbench
 * [Step-by-step guide](https://github.com/tum-lkn/NCSbench/wiki/Step-by-step-NCS-benchmarking)
+
+## Installation
+git clone https://github.com/RobinMarchart/NCSbench.git
+cd NCSbench
+source bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 if test -d ~/.pyenv
 then 
     echo "pyenv already exists"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 else
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
@@ -9,14 +11,15 @@ echo "installing dev dependencies"
 sudo apt-get update
 sudo apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
 pyenv install 3.7.5
 
 pyenv local 3.7.5
 fi
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
