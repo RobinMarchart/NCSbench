36f987862e4ffe4a73e2eaada2b360df0aaeca46
Robin Marchart <Robin.Marchart@hotmail.com>
2019-11-08 18:55:29 +0100

    added bell for sudo

--- bootstrap.bash
+++ bootstrap.bash
@@ -6,8 +6,8 @@ else
     git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
     echo "installing dev dependencies"
-    sudo apt-get update
-    sudo apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    sudo -B apt-get update
+    sudo -B apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
         libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
     export PYENV_ROOT="$HOME/.pyenv"
