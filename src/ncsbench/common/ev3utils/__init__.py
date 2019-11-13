2eecf4f0e18199895c7b015b5a0e8b87b0726214
Robin Marchart <Robin.Marchart@hotmail.com>
2019-11-13 16:33:26 +0100

    bug

--- src/ncsbench/common/ev3utils/__init__.py
+++ src/ncsbench/common/ev3utils/__init__.py
@@ -16,9 +16,9 @@ def init(ev3:str):
     motors={"A":lib.OUTPUT_A,"B":lib.OUTPUT_B,"C":lib.OUTPUT_C,"D":lib.OUTPUT_D}
     INPUT_1,INPUT_2,INPUT_3,INPUT_4,OUTPUT_A,OUTPUT_B,OUTPUT_C,OUTPUT_D=lib.INPUT_1,lib.INPUT_2,lib.INPUT_3,lib.INPUT_4,lib.OUTPUT_A,lib.OUTPUT_B,lib.OUTPUT_C,lib.OUTPUT_D
     MediumMotor,LargeMotor,TouchSensor,GyroSensor=lib.MediumMotor,lib.LargeMotor,lib.TouchSensor,lib.GyroSensor
-    if "Button" in lib.__dir__:
+    if "Button" in lib.__dir__():
         Button=lib.Button
     else:
         class B:
-            up=false
+            up=False
         Button=B
